175b5be07ecfe8b70af8b27b3fbcb30026149f3a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-25 02:12:50 +0300

    10. Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,19 +1,16 @@
-import signUpUser from './utils';
-import uploadPhoto from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    const res = {};
-    try {
-        res["photo"] = await uploadPhoto();
-    } catch (error) {
-        res["photo"] = null;
+  const res = {};
+  try {
+    res.photo = await uploadPhoto();
+  } catch (error) {
+    res.photo = null;
+  } try {
+    res.user = await createUser();
+  } catch (error) {
+    res.user = null;
+  }
 
-    }  try {
-        res["user"] = await signUpUser();
-    } catch (error) {
-        res["user"] = null;
-
-    }
-  
-    return res;
+  return res;
 }
