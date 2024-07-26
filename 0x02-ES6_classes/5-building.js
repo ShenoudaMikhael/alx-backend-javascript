3544b2524ffbb691140434dfc41d77df680b37f9
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-26 23:02:47 +0300

    5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 /* eslint-disable  no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && !Object.getOwnPropertyNames(this.constructor).find('evacuationWarningMessage')) {
+    if (this.constructor !== Building && (typeof this.evacuationWarningMessage !== 'function')) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this.sqft = sqft;
