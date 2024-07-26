0a12861cd2ae178139f9e3cf057a4400ac90d208
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-26 21:08:18 +0300

    2. A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -20,14 +20,14 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (!(name instanceof String)) {
+    if ((typeof name !== 'string') && !(name instanceof String)) {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
   set length(length) {
-    if (!(length instanceof Number)) {
+    if ((typeof length !== 'number') && !(length instanceof Number)) {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
