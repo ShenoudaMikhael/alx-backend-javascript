ebd6dd974c4f315caeb946cd76ee5ec56490f33f
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-26 23:29:14 +0300

    6. Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+/* eslint-disable  no-underscore-dangle */
+const { default: Building } = require('./5-building');
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
