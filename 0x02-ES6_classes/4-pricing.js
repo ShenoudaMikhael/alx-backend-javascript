85bc4e3acab774ca3912af5439e1df82086a2c20
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-26 22:22:55 +0300

    4. Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+/* eslint-disable  no-underscore-dangle */
+const { default: Currency } = require('./3-currency');
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    if (typeof (amount) !== 'number' && !(amount instanceof Number)) {
+      throw new TypeError('amount must be string');
+    }
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be Currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+}
