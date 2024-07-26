86d8d45e74ffdf2e6f2d2dd63221da2c395ca6f5
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-26 20:46:31 +0300

    2. A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -9,13 +9,13 @@ export default class HolbertonCourse {
     if (!(length instanceof Number)) {
       throw TypeError('Length must be a number');
     }
-    if (!(students instanceof Array)) {
-      throw TypeError('Students must be a array');
+    if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
 
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -45,8 +45,8 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!(students instanceof Array)) {
-      throw TypeError('Students must be a array');
+    if (!(students instanceof Array) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
 
     this._students = students;
