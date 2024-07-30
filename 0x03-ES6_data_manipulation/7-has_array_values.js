d7aa79a3a8e2c62520559744e77c45bdc84c94fc
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-30 16:11:47 +0300

    7. More set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,13 +1,9 @@
 export default function hasValuesFromArray(set, arr) {
-
   let res = true;
   arr.forEach(function (e) {
     if (!(set.has(e))) {
       res = false;
     }
   })
-
   return res;
-
-
 }
