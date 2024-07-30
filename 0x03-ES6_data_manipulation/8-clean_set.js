1ffb9c5188397e083bb75327eada17557d0d470d
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-07-30 16:37:08 +0300

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-
-  let q = '';
+  if (!startString) return "";
+  let q = [];
 
   set.forEach((e) => {
     if (e.startsWith(startString)) {
-      q = q + (q.length > 0 ? '-' : '') + e.substring( startString.length);
+      q.push(e.slice(startString.length));
     }
   });
-  return q;
+  return q.join('-')
 }
