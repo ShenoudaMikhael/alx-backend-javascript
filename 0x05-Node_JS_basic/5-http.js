9d32c5f05022387f8833e1f2f2e6c52241c83ead
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-18 00:29:15 +0300

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
