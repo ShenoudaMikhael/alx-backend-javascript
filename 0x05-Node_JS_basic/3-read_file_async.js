b4f22cbfc2d771ec7412970d11fe7b5ef8c6c588
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-17 23:39:08 +0300

    3. Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,24 +5,26 @@ const countStudents = (filePath) => new Promise((resolve, reject) => {
     if (err) {
       reject(new Error('Cannot load the database'));
     }
-    const byLine = data.split('\n');
-    const fullLine = byLine.filter((v) => v.length > 0);
-    const noHead = fullLine.slice(1);
-    console.log(`Number of students: ${noHead.length}`);
-    const q = {};
+    if (data) {
+      const byLine = data.split('\n');
+      const fullLine = byLine.filter((v) => v.length > 0);
+      const noHead = fullLine.slice(1);
+      console.log(`Number of students: ${noHead.length}`);
+      const q = {};
 
-    noHead.forEach((v) => {
-      const line = v.split(',');
-      if (Object.keys(q).includes(line[3])) {
-        q[line[3]].push(line[0]);
-      } else {
-        q[line[3]] = [line[0]];
-      }
-    });
-    Object.keys(q).forEach((k) => {
-      console.log(`Number of students in ${k}: ${q[k].length}. List: ${q[k].join(', ')}`);
-    });
-    resolve(true);
+      noHead.forEach((v) => {
+        const line = v.split(',');
+        if (Object.keys(q).includes(line[3])) {
+          q[line[3]].push(line[0]);
+        } else {
+          q[line[3]] = [line[0]];
+        }
+      });
+      Object.keys(q).forEach((k) => {
+        console.log(`Number of students in ${k}: ${q[k].length}. List: ${q[k].join(', ')}`);
+      });
+      resolve(true);
+    }
   });
 });
 
