dd13ce6d59c3346e2a8a74cea65279f637688af6
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-17 21:58:12 +0300

    0. Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
-const { stdout } = require('process')
+const { stdout } = require('process');
 
 function displayMessage(message) {
-  stdout.write(message + '\n');
+  stdout.write(`${message}\n`);
 }
 
 module.exports = displayMessage;
