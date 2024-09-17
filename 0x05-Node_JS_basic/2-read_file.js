ecfb30b0a8561813ffe3b5056596ff680b6d5962
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-17 23:10:56 +0300

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,31 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
+
 function countStudents(filePath) {
-    if (!fs.existsSync(filePath)) {
-        throw new Error('Cannot load the database')
+  if (!fs.existsSync(filePath)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(filePath, 'utf8');
+  const byLine = data.split('\n');
+  const fullLine = byLine.filter((v) => v.length > 0);
+  const noHead = fullLine.slice(1);
+  console.log(`Number of students: ${noHead.length}`);
+  const q = {};
+
+  noHead.forEach((v) => {
+    const line = v.split(',');
+    if (Object.keys(q).includes(line[3])) {
+      q[line[3]].push(line[0]);
+    } else {
+      q[line[3]] = [line[0]];
     }
-    const data = fs.readFileSync(filePath, 'utf8');
-    const byLine = data.split('\n')
-    const fullLine = byLine.filter(v => v.length > 0);
-    const noHead = fullLine.slice(1);
-    console.log(`Number of students: ${noHead.length}`);
-    let csCount = 0;
-    const csList = [];
-    let sweCount = 0;
-    const sweList = [];
-
-    noHead.forEach(v => {
-        const line = v.split(',');
-        if (line[3] == 'CS') {
-            csCount += 1;
-            csList.push(line[0]);
-        } else if (line[3] == 'SWE') {
-            sweCount += 1;
-            sweList.push(line[0]);
-        }
-
-    })
-
-    console.log(`Number of students in CS: ${csCount}.List: ${csList.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
+  });
+  Object.keys(q).forEach((k) => {
+    console.log(`Number of students in ${k}: ${q[k].length}. List: ${q[k].join(', ')}`);
+  });
 }
 module.exports = countStudents;
