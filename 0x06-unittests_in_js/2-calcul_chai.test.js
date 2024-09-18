38fd94c0135561a73ddccc62928e6661e914c8f9
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-18 22:06:00 +0300

    2. Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,48 @@
+const calculateNumber = require("./1-calcul");
+const { expect } = require('chai');
+describe('calculateNumber', function () {
+    describe('SUM', function () {
+        it('should return 3 when the value a and b are 1.2 and 2.2', function () {
+            expect(calculateNumber("SUM", 1.2, 2.2)).to.equal(3)
+
+        });
+
+        it('should return 7 when the value a and b are 1.2 and 5.6', function () {
+            expect(calculateNumber("SUM", 1.2, 5.6)).to.equal(7)
+
+        });
+        it('should return 6 when the value a and b are 0 and 5.6', function () {
+            expect(calculateNumber("SUM", 0, 5.6)).to.equal(6)
+
+        });
+    });
+    describe('SUBTRACT', function () {
+        it('should return -1 when the value a and b are 1.2 and 2.2', function () {
+            expect(calculateNumber("SUBTRACT", 1.2, 2.2)).to.equal(-1)
+
+        });
+
+        it('should return -5 when the value a and b are 1.2 and 5.6', function () {
+            expect(calculateNumber("SUBTRACT", 1.2, 5.6)).to.equal(-5)
+
+        });
+        it('should return -6 when the value a and b are 0 and 5.6', function () {
+            expect(calculateNumber("SUBTRACT", 0, 5.6)).to.equal(-6)
+
+        });
+    });
+    describe('DIVIDE', function () {
+        it('should return 0.5 when the value a and b are 1.2 and 2.2', function () {
+            expect(calculateNumber("DIVIDE", 1.2, 2.2)).to.equal(0.5)
+
+        });
+
+        it('should return Errorwhen the value a and b are 1.2 and 0', function () {
+            expect(calculateNumber("DIVIDE", 1.2, 0)).to.equal("Error")
+
+        });
+        it('should return 0 when the value a and b are 0 and 5.6', function () {
+            expect(calculateNumber("DIVIDE", 0, 5.6)).to.equal(0)
+        });
+    });
+});
