f172d1de2ed41305c07f387f89170c3920aa9ef6
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-18 23:02:28 +0300

    3. Spies

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,40 +1,41 @@
-const calculateNumber = require("./1-calcul");
-var assert = require('assert');
-describe('calculateNumber', function () {
-    describe('SUM', function () {
-        it('should return 3 when the value a and b are 1.2 and 2.2', function () {
-            assert.equal(calculateNumber("SUM", 1.2, 2.2), 3);
-        });
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
 
-        it('should return 7 when the value a and b are 1.2 and 5.6', function () {
-            assert.equal(calculateNumber("SUM", 1.2, 5.6), 7);
-        });
-        it('should return 6 when the value a and b are 0 and 5.6', function () {
-            assert.equal(calculateNumber("SUM", 0, 5.6), 6);
-        });
+describe('calculateNumber', () => {
+  describe('sUM', () => {
+    it('should return 3 when the value a and b are 1.2 and 2.2', () => {
+      assert.equal(calculateNumber('SUM', 1.2, 2.2), 3);
     });
-    describe('SUBTRACT', function () {
-        it('should return -1 when the value a and b are 1.2 and 2.2', function () {
-            assert.equal(calculateNumber("SUBTRACT", 1.2, 2.2), -1);
-        });
 
-        it('should return -5 when the value a and b are 1.2 and 5.6', function () {
-            assert.equal(calculateNumber("SUBTRACT", 1.2, 5.6), -5);
-        });
-        it('should return -6 when the value a and b are 0 and 5.6', function () {
-            assert.equal(calculateNumber("SUBTRACT", 0, 5.6), -6);
-        });
+    it('should return 7 when the value a and b are 1.2 and 5.6', () => {
+      assert.equal(calculateNumber('SUM', 1.2, 5.6), 7);
     });
-    describe('DIVIDE', function () {
-        it('should return 0.5 when the value a and b are 1.2 and 2.2', function () {
-            assert.equal(calculateNumber("DIVIDE", 1.2, 2.2), 0.5);
-        });
+    it('should return 6 when the value a and b are 0 and 5.6', () => {
+      assert.equal(calculateNumber('SUM', 0, 5.6), 6);
+    });
+  });
+  describe('sUBTRACT', () => {
+    it('should return -1 when the value a and b are 1.2 and 2.2', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.2, 2.2), -1);
+    });
 
-        it('should return Errorwhen the value a and b are 1.2 and 0', function () {
-            assert.equal(calculateNumber("DIVIDE", 1.2, 0), "Error");
-        });
-        it('should return 0 when the value a and b are 0 and 5.6', function () {
-            assert.equal(calculateNumber("DIVIDE", 0, 5.6), 0);
-        });
+    it('should return -5 when the value a and b are 1.2 and 5.6', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.2, 5.6), -5);
     });
+    it('should return -6 when the value a and b are 0 and 5.6', () => {
+      assert.equal(calculateNumber('SUBTRACT', 0, 5.6), -6);
+    });
+  });
+  describe('dIVIDE', () => {
+    it('should return 0.5 when the value a and b are 1.2 and 2.2', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.2, 2.2), 0.5);
+    });
+
+    it('should return Errorwhen the value a and b are 1.2 and 0', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.2, 0), 'Error');
+    });
+    it('should return 0 when the value a and b are 0 and 5.6', () => {
+      assert.equal(calculateNumber('DIVIDE', 0, 5.6), 0);
+    });
+  });
 });
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,48 +1,41 @@
-const calculateNumber = require("./2-calcul_chai");
 const { expect } = require('chai');
-describe('calculateNumber', function () {
-    describe('SUM', function () {
-        it('should return 3 when the value a and b are 1.2 and 2.2', function () {
-            expect(calculateNumber("SUM", 1.2, 2.2)).to.equal(3)
+const calculateNumber = require('./2-calcul_chai');
 
-        });
-
-        it('should return 7 when the value a and b are 1.2 and 5.6', function () {
-            expect(calculateNumber("SUM", 1.2, 5.6)).to.equal(7)
-
-        });
-        it('should return 6 when the value a and b are 0 and 5.6', function () {
-            expect(calculateNumber("SUM", 0, 5.6)).to.equal(6)
-
-        });
+describe('calculateNumber', () => {
+  describe('sUM', () => {
+    it('should return 3 when the value a and b are 1.2 and 2.2', () => {
+      expect(calculateNumber('SUM', 1.2, 2.2)).to.equal(3);
     });
-    describe('SUBTRACT', function () {
-        it('should return -1 when the value a and b are 1.2 and 2.2', function () {
-            expect(calculateNumber("SUBTRACT", 1.2, 2.2)).to.equal(-1)
 
-        });
-
-        it('should return -5 when the value a and b are 1.2 and 5.6', function () {
-            expect(calculateNumber("SUBTRACT", 1.2, 5.6)).to.equal(-5)
-
-        });
-        it('should return -6 when the value a and b are 0 and 5.6', function () {
-            expect(calculateNumber("SUBTRACT", 0, 5.6)).to.equal(-6)
-
-        });
+    it('should return 7 when the value a and b are 1.2 and 5.6', () => {
+      expect(calculateNumber('SUM', 1.2, 5.6)).to.equal(7);
     });
-    describe('DIVIDE', function () {
-        it('should return 0.5 when the value a and b are 1.2 and 2.2', function () {
-            expect(calculateNumber("DIVIDE", 1.2, 2.2)).to.equal(0.5)
-
-        });
-
-        it('should return Errorwhen the value a and b are 1.2 and 0', function () {
-            expect(calculateNumber("DIVIDE", 1.2, 0)).to.equal("Error")
-
-        });
-        it('should return 0 when the value a and b are 0 and 5.6', function () {
-            expect(calculateNumber("DIVIDE", 0, 5.6)).to.equal(0)
-        });
+    it('should return 6 when the value a and b are 0 and 5.6', () => {
+      expect(calculateNumber('SUM', 0, 5.6)).to.equal(6);
     });
+  });
+  describe('sUBTRACT', () => {
+    it('should return -1 when the value a and b are 1.2 and 2.2', () => {
+      expect(calculateNumber('SUBTRACT', 1.2, 2.2)).to.equal(-1);
+    });
+
+    it('should return -5 when the value a and b are 1.2 and 5.6', () => {
+      expect(calculateNumber('SUBTRACT', 1.2, 5.6)).to.equal(-5);
+    });
+    it('should return -6 when the value a and b are 0 and 5.6', () => {
+      expect(calculateNumber('SUBTRACT', 0, 5.6)).to.equal(-6);
+    });
+  });
+  describe('dIVIDE', () => {
+    it('should return 0.5 when the value a and b are 1.2 and 2.2', () => {
+      expect(calculateNumber('DIVIDE', 1.2, 2.2)).to.equal(0.5);
+    });
+
+    it('should return Errorwhen the value a and b are 1.2 and 0', () => {
+      expect(calculateNumber('DIVIDE', 1.2, 0)).to.equal('Error');
+    });
+    it('should return 0 when the value a and b are 0 and 5.6', () => {
+      expect(calculateNumber('DIVIDE', 0, 5.6)).to.equal(0);
+    });
+  });
 });
