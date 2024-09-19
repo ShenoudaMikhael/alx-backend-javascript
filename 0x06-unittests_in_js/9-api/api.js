c8d791683bdf9707b5e77aee586d32975ed0b5d0
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-19 23:52:07 +0300

    9. Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -7,6 +7,11 @@ const port = 7865;
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
+app.get('/cart/:id(\\d+)', (req, res) => {
+    const id = req.params.id;
+
+    res.send(`Payment methods for cart ${id}`);
+});
 
 app.listen(port, () => {
     console.log(`API available on localhost port ${port}`);
